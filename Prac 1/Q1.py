211a918197c7b7adb74d22e7fae08f3763db126a
charliekristiawan <charliekristiawan@yahoo.com>
2017-03-27 01:50:30 +0800

    Prac 1

---
+++ Prac 1/Q1.py
@@ -0,0 +1,7 @@
+def is_multiple (n,m):
+    if n % m == 0:
+        print("True")
+    else:
+        print("False")
+
+is_multiple(10.5)
---
+++ Prac 1/Q2.py
@@ -0,0 +1,3 @@
+n = 8
+list = [2**k for k in range(0,n+1)]
+print(list)
